b98e923f0743c3bf7146392eed06e0c02bdfa048
MiaMou <19845509@qq.com>
2021-01-20 16:02:43 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,111 @@
+console.log('main.js');
+
+// AJAX 加载 CSS 全过程
+getCSS.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET', '/style.css')   // readyState === 1
+    request.onreadystatechange = ()=>{
+        // console.log(request.readyState)
+        // 下载完成，但不知道是成功 2xx 还是失败 4xx
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                // 创建 style 标签   
+                const style = document.createElement('style')
+                // 填写 style 内容
+                style.innerHTML = request.response
+                // 插到头里面
+                document.head.appendChild(style)
+            } else {
+                alert('加载 CSS 失败')
+            }        
+        }        
+    }
+    request.send()
+}
+
+getJS.onclick = ()=>{
+    const request = new XMLHttpRequest();
+    request.open("GET", "/2.js")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                const script = document.createElement('script')
+                script.innerHTML = request.response
+                document.body.appendChild(script)
+            } else {
+                alert('加载 JS 失败')
+            }        
+        }        
+    }
+    request.send()
+}
+
+getHTML.onclick = ()=>{
+    const request = new XMLHttpRequest();
+    request.open("GET", "/3.html")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                const div = document.createElement('div')
+                div.innerHTML = request.response
+                document.body.appendChild(div)
+            } else {
+                alert('加载 HTML 失败')
+            }        
+        }        
+    }
+    request.send()
+}
+
+getXML.onclick = ()=>{
+    const request = new XMLHttpRequest();
+    request.open("GET", "/4.xml")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                const dom = request.responseXML
+                const text = dom.getElementsByTagName('warning')[0].textContent
+                console.log(text.trim()) 
+            } else {
+                alert('加载 XML 失败')
+            }        
+        }        
+    }
+    request.send()
+}
+getJSON.onclick = ()=>{
+    const request = new XMLHttpRequest();
+    request.open("GET", "/5.json")
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                console.log(request.response);  // 打印出的响应是字符串
+                const object = JSON.parse(request.response)  // 把符合json语法的字符串变成JS对应的对象
+                myName.textContent = object.name
+            } else {
+                alert('加载 JSON 失败')
+            }        
+        }        
+    }
+    request.send()
+}
+
+let n = 1
+getPage.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET', `/page${n+1}`)
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                const array = JSON.parse(request.response)
+                array.forEach(item => {
+                    const li = document.createElement('li')
+                    li.textContent = item.id
+                    xxx.appendChild(li)
+                })
+                n += 1
+            }
+        }
+    }
+    request.send()
+}
